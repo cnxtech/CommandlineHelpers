85b7e4e01e3561b00362e4a6c0db51adfa5f56f1
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2015-01-31 15:28:36 +0100

    Initital Commit

---
+++ create_cords_for_transactions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import csv
+
+from clint import resources
+
+from utils import oauth_login, base_url, bank_id, account_id
+
+resources.init('openbankproject', 'evmakesgeo')
+key_file = resources.user.read('config.ini')
+
+openbank = oauth_login(base_url, key_file)
+
+# Parse cords from csv
+reader = csv.reader(open('static/ireland_points.csv'))
+firstline = True
+csv_data = []
+for row in reader:
+    if firstline:    #skip first line
+        firstline = False
+        continue
+    csv_data.append((row[0], row[1]))
+
+# Get transactoin ids
+response = openbank.get("{}obp/v1.2.1/banks/{}/accounts/{}/owner/transactions".format(base_url, bank_id, account_id))
+transactions = [ each['id'] for each in response.json()['transactions']][0:len(csv_data)]
+
+
+# Create for a list of transactions id geo metadata
+for index, transaction in enumerate(transactions):
+    url = "{}obp/v1.2.1/banks/{}/accounts/{}/public/transactions/{}/metadata/where".format(base_url, bank_id, account_id, transaction)
+    payload = '{"where": { "latitude":' + csv_data[index][1] + '"longitude": ' + csv_data[index][0] + ' } }'
+    headers = {'content-type': 'application/json'}
+    res = openbank.post(url, data=payload, headers=headers)
---
+++ utils.py
@@ -0,0 +1,54 @@
+import json
+
+from clint import resources
+from clint.textui import colored, puts
+
+from requests_oauthlib import OAuth1Session
+
+base_url = 'https://ulsterbank.openbankproject.com/'
+bank_id = 'ulster'
+account_id = 'charity1'
+
+client_key = ""
+client_secret = ""
+
+def oauth_login(base_url, key_file):
+
+    openbank = None
+
+    # Make Oauth
+    if not key_file:
+        request_token_url = "{}oauth/initiate".format(base_url)
+        authorization_base_url = "{}oauth/authorize".format(base_url)
+        access_token_url = "{}oauth/token".format(base_url)
+
+        openbank = OAuth1Session(
+            client_key,
+            client_secret=client_secret,
+            callback_uri='http://127.0.0.1/cb'
+        )
+
+        openbank.fetch_request_token(request_token_url)
+
+        authorization_url = openbank.authorization_url(authorization_base_url)
+
+        puts(colored.blue('Please go here and authorize:'))
+        puts(colored.green(authorization_url))
+        puts()
+
+        redirect_response = input('Paste the full redirect URL here:')
+        openbank.parse_authorization_response(redirect_response)
+        oauth_tokens = openbank.fetch_access_token(access_token_url)
+        resources.user.write('config.ini', json.dumps(oauth_tokens))
+    else:
+        keys = json.loads(key_file)
+        openbank = OAuth1Session(
+            client_key,
+            client_secret=client_secret,
+            resource_owner_key=keys.get('oauth_token'),
+            resource_owner_secret=keys.get('oauth_token_secret')
+        )
+
+    puts(colored.yellow("Login Done"))
+
+    return openbank
