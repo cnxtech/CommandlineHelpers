c7372552c695bdfb8ba9cf828ec7b23136e62cab
Simon Redfern <simon@tesobe.com>
2015-02-01 14:19:00 +0000

    adding create trans

---
+++ create_transaction.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import csv
+
+from clint import resources
+
+from utils import oauth_login, base_url, bank_id, account_id
+
+resources.init('openbankproject', 'evmakesgeo')
+key_file = resources.user.read('config.ini')
+
+openbank = oauth_login(base_url, key_file)
+
+# Get transactions ids
+response = openbank.get("{}obp/v1.3.0/banks/{}/accounts/{}/owner/transactions".format(base_url, bank_id, account_id))
+#print "{}obp/v1.2.1/banks/{}/accounts/{}/owner/transactions".format(base_url, bank_id, account_id)
+transactions = [ each['id'] for each in response.json()['transactions']]
+
+print transactions
+
+
+url = "{}obp/v1.2.1/banks/{}/accounts/{}/owner/transactions".format(base_url, bank_id, account_id)
+
+bank_id = 'ulster'
+other_account_id = "personal5"
+amount = 100
+
+payload = '{"account_id" : "' + other_account_id + '", "bank_id": "' + bank_id + '", "amount": "' + str(amount) + '"}'
+
+
+print 'url is: %s' % url
+print 'payload is: %s' % payload
+
+
+headers = {'content-type': 'application/json'}
+print ('before post')
+
+res = openbank.post(url, data=payload, headers=headers)
+
+print res
